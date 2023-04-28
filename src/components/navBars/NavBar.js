0f2900013919e56713f8496e17da77878f6cdf9f
firasjedidi <firasjedidi.com>
2023-04-28 14:06:05 +0100

    pushing the navbars

--- src/components/navBars/NavBar.js
+++ src/components/navBars/NavBar.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import style from '../../styles/navBars.module.css';
- import { BsList } from "react-icons/bs";
+ import { BsList ,BsSearch , BsBell} from "react-icons/bs";
 const NavBar = () => {
   return (
     <nav className={style['nav-bar-container']}>
       <div className={style['nav-bar-fulid']}>
-        <div className={style['container-fluid']}>
-          <BsList className={style['app-sidebar-toggle']}/>
-         
-        </div>
+        <section className={style['nav-bar-fulid']}>
+          <BsList className={style['nav-bar-toggle-icon']}/>
+          <section className={style['nav-bar-search-container']}>
+            <BsSearch className={style['nav-bar-search-icon']}/>
+            <input type='search' placeholder='Search pathology results'  className={style['nav-bar-search-input']} />
+          </section>
+        </section>
+        <section className={style['nav-bar-side-container']}>
+          <BsBell className={style['nav-bar-notification-icon']}/>
+          <section  className={style['nav-bar-profile-container']}>
+            <img className={style['nav-bar-profile-img']} />
+            <span className={style['nav-bar-profile-name']} >
+              firas jedidi
+            </span>
+          </section>
+        </section>
       </div>
     </nav>
   )
